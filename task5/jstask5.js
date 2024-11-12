9ffe13488450a201437ffc533c4fcf44b85820aa
oknyabul <oknyabul-05@mail.ru>
2024-11-12 13:43:05 +0300

    Update jstask5.js

--- task5/jstask5.js
+++ task5/jstask5.js
@@ -2,12 +2,12 @@ document.addEventListener("DOMContentLoaded", () => {
     const productSelect = document.querySelector("#product");
     const quantityInput = document.querySelector("#quantity");
     const resultDiv = document.querySelector("#result");
-    const numberRegex = /^\d*$/
+    const numberRegex = /^[1-9]\d*$/
     document.querySelector(".button").addEventListener("click", () => {
         const quantity = parseInt(quantityInput.value);
         const pricePerItem = parseFloat(productSelect.value);
 
-        if (!numberRegex.test(input.value.trim()) || input.value.trim() === "") {
+        if (!numberRegex.test(quantity)) {
             resultDiv.textContent = "Ошибка: введите корректное количество.";
             return;
         }
