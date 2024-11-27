314ac28e25da3753e9c5b08523d27cfb4164c883
oknyabul <oknyabul-05@mail.ru>
2024-11-27 17:10:46 +0300

    Add files via upload

---
+++ task7/jstask7.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+  var $gallery = $('.gallery');
+  
+  // Инициализация слайдера
+  $gallery.slick({
+      centerMode: true,
+      centerPadding: 0,
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      dots: true,
+      infinite: true, // Бесконечный слайдер
+      responsive: [
+          {
+              breakpoint: 576,
+              settings: {
+                  slidesToShow: 1,
+                  centerMode: false,
+              }
+          }
+      ]
+  });
+});
